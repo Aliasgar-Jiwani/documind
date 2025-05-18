0123bc65b70716ae0bb134cb5f18139afeccb42f
Aliasgar Jiwani <aliasgarjiwani@gmai.com>
2025-05-18 16:55:44 +0530

    Remove unused imports

--- backend.py
+++ backend.py
@@ -1,10 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import httpx
 import logging
-import re
-import os
 import json
 import asyncio
 from typing import Optional, List
